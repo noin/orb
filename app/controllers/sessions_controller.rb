6487472f9c809137dd2ebdec7ecffbc18cf56411
Jason Ketterman <jason.ketterman@mac.com>
2008-03-02 08:13:25 -0600

    Updating comments. Testing branching.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,20 +3,20 @@ class SessionsController < ApplicationController
   # - GET /login
   # Renders a login form
   def new
-    
+    # Display views/sessions/new.rhtml.erb
   end
   
   # - POST /session
   # Handles OpenID Authentication
   def create
-    
+    # Redirect to destination or #new if the auth fails.
   end
   
   # - DELETE /session
   # - GET /logout
   # Destroys the user's session
   def destroy
-    
+    # Redirect to home_url
   end
   
 end
