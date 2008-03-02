7f9d70dea37fbb4e113b182bddccd6d5ef34e440
Jason Ketterman <jason.ketterman@mac.com>
2008-03-02 08:50:25 -0600

    Adding microid helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  require 'digest/sha1'
+
+  def microid(email, site = nil)
+    site ||= request.protocol + request.host_with_port + request.path
+    microid = Digest::SHA1.hexdigest(Digest::SHA1.hexdigest("mailto:#{email}") + Digest::SHA1.hexdigest(site))
+    tag :meta, :name => "microid", :content => microid
+  end  
 end
