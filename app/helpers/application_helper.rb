00b1518d318fcb0c25fd39d85d8d985f58795d24
Noin <nclaude@gmail.com>
2008-03-18 17:59:29 +0200

    Fixed microid tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 	def microid(email, site = nil)
 		site ||= request.protocol + request.host_with_port + request.path
 		microid = Digest::SHA1.hexdigest(Digest::SHA1.hexdigest("mailto:#{email}") + Digest::SHA1.hexdigest(site))
-		tag :meta, :name => "microid", :content => microid
+		tag :meta, :name => "microid", :content => 'mailto+http:sha1:' + microid
 	end
 
 	# gravatar_url_for 'greenisus@gmail.com', :rating => 'R', :width => 80, :height => 80
